e3ff0b9076e00b853a0567f634caa85d17b781b9
Vishnu Ramesh <Bablu@Vishnus-MacBook-Air.local>
2020-02-10 21:13:18 +0000

    html plugin

--- _webpack/webpack.common.js
+++ _webpack/webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -8,6 +9,12 @@ module.exports = {
     path: path.resolve(__dirname, '../_dist'),
     filename: '[name].bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My website',
+      template: path.resolve(__dirname, '../_templates/client.html'),
+    }),
+  ],
   module: {
     rules: [
       {
